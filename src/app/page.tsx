4d43f777381b52ef15e0bac402922c52ab4627e4
theridwanade <theridwanade@gmail.com>
2026-02-24 13:01:42 +0100

    Initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (<div>
+    This is mPDF, a pdf design tool for creating professional-looking PDFs. With mPDF, you can easily create PDFs with custom layouts, styles, and content. Whether you're a designer, developer, or marketer, mPDF has everything you need to create stunning PDFs quickly and easily.
+  </div>)
+}
